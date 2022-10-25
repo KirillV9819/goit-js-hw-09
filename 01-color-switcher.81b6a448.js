e42c242e9f1c446bc86d7bc2d01c9fee5d73fe7f
KirillV9819 <105566727+KirillV9819@users.noreply.github.com>
2022-10-25 19:15:53 +0000

    Deploying to gh-pages from @ KirillV9819/goit-js-hw-09@e83bac51ac334ab31965a1a93c934cac4f19d85e 🚀

---
+++ 01-color-switcher.81b6a448.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("body"),e=document.querySelector("button[data-start]"),d=document.querySelector("button[data-stop]");d.disabled=!0,e.addEventListener("click",(function(){timerId=setInterval((function(){t.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),e.disabled=!0,d.disabled=!1})),d.addEventListener("click",(function(){clearInterval(timerId),e.disabled=!1,d.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.81b6a448.js.map
